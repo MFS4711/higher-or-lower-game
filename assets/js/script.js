671a5b676bf0d2d2e9e31204fb9c31935773d6fe
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-07 14:08:53 +0000

    Add basic js structure: - create random numbers - create functions that will likely be required - link buttons and check they work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,46 @@
-console.log("hello");
+document.addEventListener("DOMContentLoaded", function() {
+
+    runGame();
+});
+
+function runGame () {
+
+    // create a random number for user to start with and add to HTML
+    let num1 = Math.ceil(Math.random() * 100);
+    document.getElementById("player-number").innerText = num1;
+
+    // create initial num 2 ? and add to HTML
+    let num2 = "?";
+    document.getElementById("computer-number").innerText = num2;
+
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "higher") {
+                higher();
+            } else if (this.getAttribute("data-type") === "lower") {
+                lower();
+            }
+        })
+    }
+}
+
+function higher () {
+
+}
+
+function lower () {
+
+}
+
+function chackAnswer () {
+
+}
+
+function incrementScore () {
+
+}
+
+function displayHighScore () {
+
+}
