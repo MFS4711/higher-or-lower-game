4fdc736127572e4cc7b44901b8a2913309fd5f26
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-11-08 09:24:48 +0000

    bug fix on bum1 becoming ? - - amend code to make num1 a new random number instead of the last num2 - resolves issue - new bug related to score is outstanding

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,11 +108,14 @@ function displayHighScore () {
 
 function continueGame () {
 
-    let num1 = document.getElementById("computer-number").innerText;
+    // originally wanted the num1 to equal the last num2 but issues where num1 beomes ?
+
+    // let num1 = document.getElementById("computer-number").innerText;
+    let num1 = Math.ceil(Math.random() * 100);
     document.getElementById("player-number").innerText = num1;
 
-    let num2 = "?";
-    document.getElementById("computer-number").innerText = num2;
+    // let num2 = "?";
+    document.getElementById("computer-number").innerText = "?";
 
     runGame();
 
